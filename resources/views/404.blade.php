5a66c83ef21d542e1f9ca09f50f6c5ef8c1692b0
kuvinci <kuvinci@gmail.com>
2024-03-12 19:35:46 +0200

    Added 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,8 +1,9 @@
 @include('layouts.header')
 
 <div class="container shadow-sm p-3 bg-white rounded-3">
-    <h1>Redirect limit reached</h1>
+    <h1>{{$title}}</h1>
     <h4>(I'm too lazy to style this page for a test task, sorry)</h4>
+    <h5>¯\_(ツ)_/¯</h5>
 </div>
 
 @include('layouts.footer')
