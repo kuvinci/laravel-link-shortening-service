a48f6c74830fa7e37064266a0830571617012e10
kuvinci <kuvinci@gmail.com>
2024-03-12 19:40:29 +0200

    Small margin top on 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,6 +1,6 @@
 @include('layouts.header')
 
-<div class="container shadow-sm p-3 bg-white rounded-3 d-flex flex-column align-items-center">
+<div class="container mt-5 shadow-sm p-3 bg-white rounded-3 d-flex flex-column align-items-center">
     <h1>{{$title}}</h1>
     <h4>(I'm too lazy to style this page for a test task, sorry)</h4>
     <h5>¯\_(ツ)_/¯</h5>
